fb5b4ef6ba5a5211ddc76e57fced9c33dd6569b5
rasmi <rasminoel@gmail.com>
2023-10-27 12:29:13 +0300

    Done With Assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,13 +4,20 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+  function handleLiClick(id) {
+    const newFoodList = foods.filter((food) => food.id !== id);
+    setFoods(newFoodList);
+  }
+
+
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodList = [...foods, newFood];  // spread operator
+    setFoods(newFoodList);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)} >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -19,6 +26,7 @@ function SpicyFoodList() {
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+
     </div>
   );
 }
